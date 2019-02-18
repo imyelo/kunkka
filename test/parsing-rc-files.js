db9e83475d3d9b27c7aaacf5a8f0d8a9a8e2af9e
yelo <zhihuzeye@gmail.com>
2019-02-18 18:53:26 +0800

    test: parsing cjs rc

--- test/parsing-rc-files.js
+++ test/parsing-rc-files.js
@@ -8,7 +8,7 @@ test.beforeEach(setup)
 
 test.afterEach.always(teardown)
 
-async function macro (t, [filename, content, expected]) {
+async function macro (t, [filename, content], verify) {
   const { cwd } = t.context
   const spy = sinon.spy()
 
@@ -26,7 +26,7 @@ async function macro (t, [filename, content, expected]) {
 
   await run(t, Cli, BuildCommand, 'build')
 
-  t.true(spy.calledWith(expected), 'config should be parsed')
+  await verify(t, spy)
 
   t.pass()
 }
@@ -34,41 +34,50 @@ async function macro (t, [filename, content, expected]) {
 test.serial('parse .{appname}rc using json', macro, [
   '.clirc',
   '{ "name": "hi" }',
-  { name: 'hi' },
-])
+], (t, spy) => {
+  t.true(spy.calledWith({ name: 'hi' }), 'config should be parsed')
+})
 
 test.serial('parse .{appname}rc using yaml', macro, [
   '.clirc',
   'name: hi\n',
-  { name: 'hi' },
-])
+], (t, spy) => {
+  t.true(spy.calledWith({ name: 'hi' }), 'config should be parsed')
+})
 
 test.serial('parse .{appname}rc.json', macro, [
   '.clirc.json',
   '{ "name": "hi" }',
-  { name: 'hi' },
-])
+], (t, spy) => {
+  t.true(spy.calledWith({ name: 'hi' }), 'config should be parsed')
+})
 
 test.serial('parse .{appname}rc.yaml', macro, [
   '.clirc.yaml',
   'name: hi\n',
-  { name: 'hi' },
-])
+], (t, spy) => {
+  t.true(spy.calledWith({ name: 'hi' }), 'config should be parsed')
+})
 
 test.serial('parse .{appname}rc.yml', macro, [
   '.clirc.yml',
   'name: hi\n',
-  { name: 'hi' },
-])
+], (t, spy) => {
+  t.true(spy.calledWith({ name: 'hi' }), 'config should be parsed')
+})
 
 test.serial('parse .{appname}rc.js', macro, [
   '.clirc.js',
-  'module.exports = { "name": "hi" }',
-  { name: 'hi' },
-])
+  'module.exports = { "name": "hi", "foo": function () { return "bar" } }',
+], (t, spy) => {
+  t.true(spy.calledWithMatch({ name: 'hi' }), 'config should be parsed')
+  t.is(spy.lastCall.lastArg.foo(), 'bar')
+})
 
 test.serial('parse {appname}.config.js', macro, [
   'cli.config.js',
-  'module.exports = { name: "hi" }',
-  { name: 'hi' },
-])
+  'module.exports = { "name": "hi", "foo": function () { return "bar" } }',
+], (t, spy) => {
+  t.true(spy.calledWithMatch({ name: 'hi' }), 'config should be parsed')
+  t.is(spy.lastCall.lastArg.foo(), 'bar')
+})
